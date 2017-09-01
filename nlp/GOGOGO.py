2fd3f34407f7b0ada4a37341d8a7530363c78e4c
andy19960129 <andy19960129@gmail.com>
2017-09-01 21:47:35 +0800

    Add files via upload

--- nlp/GOGOGO.py
+++ nlp/GOGOGO.py
@@ -7,8 +7,6 @@ def go_go_go(num: int)-> None:
 
     with mongodb.Mongodb() as db:
 
-        db.db["record"].remove({"the標題": "tf_idf_dict"})
-
         original_db_data = db.db_all("articles")
         jie_ba_db_data = db.db_all("jie_ba_Articles")
 
@@ -24,33 +22,15 @@ def go_go_go(num: int)-> None:
                 db.insert_one("jie_ba_Articles", jie_ba_return)
                 print("{0}/{1} finished!".format(i, a))
 
-    get_JIEBA.up_dict()
-
 
 def interface(search_key: str)->list:
 
     with mongodb.Mongodb() as db:
-        '''
-        original_db_data = db.db_all("articles")
-        jie_ba_db_data = db.db_all("jie_ba_Articles")
-
-        if len(jie_ba_db_data) < len(original_db_data):
-            print('original articles has {0} , jie ba articles has {1}'.format(len(original_db_data),
-                                                                               len(jie_ba_db_data)))
-            an = input('press number 0 to update or any key to skip')
-
-            if an == 0:
-                for i in range(len(jie_ba_db_data), len(original_db_data)):
-                    jie_ba_return = get_JIEBA.get_jie_ba(original_db_data[i]["content"])
-                    jie_ba_return["title"] = original_db_data[i]["title"]
-                    db.insert_one("jie_ba_Articles", jie_ba_return)
-                    print("{0}/{1} finished!".format(i, len(original_db_data)))
-        '''
-        tf_idf_dict = get_JIEBA.get_tf_idf()
 
         articles_list = db.search_title("articles", search_key)
         jie_ba_articles_list = db.search_title("jie_ba_Articles", search_key)
 
+        x = 1
         for a in jie_ba_articles_list:
             w_num = 0
             count = 99
@@ -74,12 +54,12 @@ def interface(search_key: str)->list:
             a["date_added"] = articles_list[int(temp[0])]["date_added"]
             a["content"] = articles_list[int(temp[0])]["content"]
 
-        x = 1
-        for article in jie_ba_articles_list:
+            tf_idf_dict = get_JIEBA.get_tf_idf(a["content"])
+
             encode = []
-            for word in article["segments"]:
+            for word in a["segments"]:
                 encode.append(tf_idf_dict[word])
-            article["encoded"] = encode
+            a["encoded"] = encode
             print("{0}/{1} encoded".format(x, len(jie_ba_articles_list)))
             x += 1
 
--- nlp/get_JIEBA.py
+++ nlp/get_JIEBA.py
@@ -3,88 +3,26 @@ from crawler_api import crawler
 from crawler_api import mongodb
 import jieba
 import jieba.posseg as pseg
-import math
+# import math
 from os import path
-import operator
-
-
-# 產生"tf_dict.txt"
-def tf_dict_first_process()->None:
-    with mongodb.Mongodb() as db:
-        d = db.search_any("record", "the標題", "tf_dict")
-        if not d:
-            c = {"THE總共": 0, "the標題": "tf_dict"}
-            crawler.json_write("tf_dict.txt", c)
-        else:
-            del d[0]['_id']
-            crawler.json_write("tf_dict.txt", d[0])
-
-
-# 產生"idf_dict.txt"
-def idf_dict_first_process()->None:
-    with mongodb.Mongodb() as db:
-        d = db.search_any("record", "the標題", "idf_dict")
-        if not d:
-            c = {"THE總共": 0, "the標題": "idf_dict"}
-            crawler.json_write("idf_dict.txt", c)
-        else:
-            del d[0]['_id']
-            crawler.json_write("idf_dict.txt", d[0])
+# import operator
 
 
 # 計算tf_idf
-def get_tf_idf()->dict:
+def get_tf_idf(str_list: list)->dict:
 
     with mongodb.Mongodb() as db:
 
         tf_idf = db.search_any("record", "the標題", "tf_idf_dict")
         if tf_idf:
-            return tf_idf
+            tf_idf_dict = tf_idf[0]
+        else:
+            tf_idf_dict = {"the標題": "tf_idf_dict"}
 
-        tf_dict = {"THE總共": 0}
-        idf_dict = {"THE總共": 0}
-        jie_ba_articles_list = db.db_all("jie_ba_Articles")
-
-        temp = []
-
-        for one_articles in jie_ba_articles_list:
-            jie_ba_word_list = one_articles["segments"]
-
-            for word in jie_ba_word_list:
-                if word in tf_dict:  # 計算出現次數 與 總辭數
-                    tf_dict[word] += 1
-                else:
-                    tf_dict[word] = 1
-                tf_dict["THE總共"] += 1
-                if word not in temp:  # 計算出現文章數
-                    temp.append(word)
-
-            for w in temp:
-                if w in idf_dict:
-                    idf_dict[w] += 1
-                else:
-                    idf_dict[w] = 1
-            idf_dict["THE總共"] += 1
-            temp.clear()
-
-        for i in idf_dict:
-            if i != "THE總共" and i != "the標題":
-                idf_dict[i] = 1 - (idf_dict[i] / (idf_dict["THE總共"] + 1))
-                # idf_dict[i] = math.log10(idf_dict["THE總共"]+1 / idf_dict[i])
-
-        tf_idf_dict = {"the標題": "tf_idf_dict"}
-
-        for i in tf_dict:
-            if i != "THE總共" and i != "the標題":
-                # tf_idf_dict[i] = math.log10(tf_dict[i] * idf_dict[i])
-                tf_idf_dict[i] = math.log10(tf_dict[i]) * idf_dict[i]
-
-        tf_idf_list = sorted(tf_idf_dict.items(), key=operator.itemgetter(1), reverse=True)
-
-        x = 1
-        for key, value in tf_idf_list:
-            if key != "THE總共":
-                tf_idf_dict[key] = x
+        x = len(tf_idf_dict)
+        for word in str_list:
+            if word not in tf_idf_dict:
+                tf_idf_dict[word] = x
                 x += 1
 
         crawler.json_write("tf_idf_dict.txt", tf_idf_dict)
@@ -117,27 +55,3 @@ def get_jie_ba(string: str)->dict:
     ans = {"segments": word, "pos": flag}
 
     return ans
-
-
-# 上傳tf_dict and idf_dict
-def up_dict()->None:
-    tf_dict = crawler.json_read("tf_dict.txt")
-    idf_dict = crawler.json_read("idf_dict.txt")
-
-    with mongodb.Mongodb() as db:
-        db.db["record"].remove({"the標題": "tf_dict"})
-        db.db["record"].remove({"the標題": "idf_dict"})
-        db.insert_one("record", tf_dict)     # !!!!!!!!!!需更新的function
-        db.insert_one("record", idf_dict)    # !!!!!!!!!!
-
-
-# Frequency_dict_least_process()
-# tf_idf_dict_least_process()
-
-# up_dict()
-'''
-tf_dict_first_process()
-idf_dict_first_process()
-x= Get_jieba("還有開大卡車送貨，偶而回家的爸爸住在一個小小的公寓裡頭，麥諾利多是個成績平平的平凡小孩，他的媽媽是典型的望子成龍型家長")
-print(x)
-'''
